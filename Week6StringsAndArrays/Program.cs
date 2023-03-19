53dce1c944132186d2def5aa2d6fa07dc0849573
Mishael <mishael.hussain@tktk.ee>
2023-03-19 19:19:11 +0200

    add week2 code

---
+++ Week6StringsAndArrays/Program.cs
@@ -0,0 +1,31 @@
+﻿string hello = " we are the champions?";
+
+int stringLenght = hello.Length;
+
+
+hello = hello.Trim();
+
+char firstLetter = hello[0]; 
+Console.WriteLine(firstLetter);
+
+hello = hello.Replace(hello[0], char.ToUpper(hello[0])).Replace('?', '!');
+
+firstLetter= hello[0]; 
+Console.WriteLine(firstLetter);
+
+Console.WriteLine(hello);
+
+
+
+
+
+//int wordCounter = 1; 
+//for(int i = 0; i < hello.Length; i++)
+//{
+//    if (hello[i] == ' ')
+//    {
+//        wordCounter++;
+//    }
+//}
+
+//Console.Write($"Word count in {hello}: {wordCounter + 1} words.");
